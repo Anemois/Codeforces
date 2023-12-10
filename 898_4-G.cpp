874eea4eeb2309ae161ef9042857df904ae4ecfe
Anemois <danieltang1668@gmail.com>
2023-12-10 10:55:09 +0800

    2023/12/10

--- 898_4-G.cpp
+++ 898_4-G.cpp
@@ -1,12 +1,26 @@
-//2023/12/9 difficulty:1500
+//2023/12/10 difficulty:1500
 #include <bits/stdc++.h>
 #pragma GCC optimize("O2")
 #define Anemoi ios_base::sync_with_stdio(false);cin.tie(0);
 #define int long long
 using namespace std;
 
-bool solve(){
-    
+int solve(){
+    string str;
+    cin >> str;
+    int n = str.length(), all=0, mn=INT_MAX, cnt=0;
+    for(int i=0; i<n; i++){
+        if(str[i] == 'A'){
+            all++;
+            cnt++;
+        }
+        if(str[i] == 'B'){
+            mn = min(mn, cnt);
+            cnt = 0;
+        }
+    }
+    mn = min(mn, cnt);
+    return max(0LL, all-mn);
 }
 
 signed main(){
@@ -14,8 +28,5 @@ signed main(){
     int t = 1;
     cin >> t;
     for(int i=0; i<t; i++)
-        if(solve())
-            cout << "YES\n";
-        else
-            cout << "NO\n";
+        cout << solve() << "\n";
 }
