2302a10e6a495d8ce8302f8d92d8b708e4ebef11
Anemois <danieltang1668@gmail.com>
2023-12-26 22:12:33 +0800

    template change added #if

--- template.cpp
+++ template.cpp
@@ -1,18 +1,33 @@
-//2023/XX/XX
+//2023/12/25 difficulty:1600
 #include <bits/stdc++.h>
-#pragma GCC optimize("O2")
 #define Anemoi ios_base::sync_with_stdio(false);cin.tie(0);
 #define int long long
+#define HAS_T true
+#define MAIN void
 using namespace std;
 
-void solve(){
-    ;
+MAIN solve(){
+	
 }
 
 signed main(){
-    Anemoi
-    int t = 1;
-    cin >> t;
-    for(int i=0; i<t; i++)
-        solve();
+	Anemoi
+	int t;
+	#if HAS_T == true
+	cin >> t;
+	#endif
+	while(t--){
+		#if MAIN == void
+		solve();
+		#elif MAIN == bool
+		if(solve())
+			cout << "YES\n";
+		else
+			cout << "NO\n";
+		#else
+		cout << solve() << "\n";
+		#endif
+	}
+		
+	return 0;
 }
