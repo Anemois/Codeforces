1890af2438eb553e807755ec13980a2af3c6cf4f
Anemois <danieltang1668@gmail.com>
2023-12-26 22:34:49 +0800

    what am i doing to my template

--- template.cpp
+++ template.cpp
@@ -1,12 +1,19 @@
 //2023/12/25 difficulty:1600
 #include <bits/stdc++.h>
-#define Anemoi ios_base::sync_with_stdio(false);cin.tie(0);
-#define int long long
+
 #define HAS_T true
 #define MAIN void
+
+#define Anemoi ios_base::sync_with_stdio(false);cin.tie(0);
+#define int long long
 using namespace std;
 
-MAIN solve(){
+#if MAIN == INT
+int
+#else
+MAIN
+#endif
+solve(){
 	
 }
 
